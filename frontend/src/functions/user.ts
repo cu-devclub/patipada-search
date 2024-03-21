1d3d74f0f4d54c1f8aefe62ef88967d325f66a26
Waris Lakthong <77393871+spyspoisspuis@users.noreply.github.com>
2024-03-21 23:03:14 +0700

    User (admin view) page and components

--- frontend/src/functions/user.ts
+++ frontend/src/functions/user.ts
@@ -1,5 +1,6 @@
 import { getCookie, removeCookie } from "typescript-cookie";
 import { verifyToken, authorize } from "../service/user";
+import { RoleMapValue } from "../constant";
 export const SignOut = () => {
   removeCookie("token");
   removeCookie("username");
@@ -52,4 +53,16 @@ export const AuthorizeAdmin = async (requireRole: string) => {
       });
   }
   return ch;
+};
+
+export const CheckRoleDisplayDeleteButton = (
+  role: string,
+  displayRole: string
+) => {
+  const roleValue = RoleMapValue[role];
+  const displayRoleValue = RoleMapValue[displayRole];
+  if (roleValue > displayRoleValue) {
+    return true;
+  }
+  return false;
 };
